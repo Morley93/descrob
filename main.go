bf8674db4de004fb73451b4b0c46a3dcd49396be
John Morley <git@johnmorley.uk>
2020-10-30 16:37:36 +0000

    Mostly refactor web stuff

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"net/http/cookiejar"
-	"net/url"
 	"os"
-	"strings"
 )
 
 type Track struct {
@@ -28,32 +25,24 @@ type RecentTrackResp struct {
 	} `json:"recenttracks"`
 }
 
-var client *http.Client
-
 func main() {
 	username := os.Getenv("LASTFM_USERNAME")
 	password := os.Getenv("LASTFM_PASSWORD")
 	apiKey := os.Getenv("LASTFM_API_KEY")
 
-	kl, err := os.OpenFile("keys.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
-	if err != nil {
-		panic(err)
-	}
-	_ = &http.Transport{
-		TLSClientConfig: &tls.Config{KeyLogWriter: kl, InsecureSkipVerify: true},
-	}
-
-	jar, err := cookiejar.New(nil)
-	if err != nil {
-		panic(err)
-	}
-	client = &http.Client{Jar: jar}
-
 	recents := getRecentTracks(username, apiKey)
 
-	startSession(username, password)
+	webClient, err := NewLastFMWebClient(username, password)
+	if err != nil {
+		log.Fatalf("Failed to create a LastFM web client: %v", err)
+	}
 
-	doDelete(recents.RecentTracks.Tracks[0], username)
+	trackToDelete := recents.RecentTracks.Tracks[0]
+	err = webClient.DeleteTrack(trackToDelete)
+	if err != nil {
+		log.Fatalf("Track deletion failed: %v", err)
+	}
+	fmt.Println("Deleted track", trackToDelete)
 }
 
 func getRecentTracks(username, apiKey string) RecentTrackResp {
@@ -69,7 +58,7 @@ func getRecentTracks(username, apiKey string) RecentTrackResp {
 
 	req.URL.RawQuery = q.Encode()
 
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
@@ -83,72 +72,4 @@ func getRecentTracks(username, apiKey string) RecentTrackResp {
 		panic(err)
 	}
 	return decoded
-}
-
-func startSession(username, password string) {
-	loginURL := "https://secure.last.fm/login"
-	_, err := client.Get(loginURL)
-
-	csrfCookie := findCookieForDomain(client.Jar, "https://secure.last.fm", "csrftoken")
-
-	form := url.Values{}
-	form.Add("username_or_email", username)
-	form.Add("password", password)
-	form.Add("csrfmiddlewaretoken", csrfCookie.Value)
-	req, err := http.NewRequest(http.MethodPost, loginURL, strings.NewReader(form.Encode()))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Referer", loginURL)
-	resp, err := client.Do(req)
-	fmt.Println(resp)
-	if err != nil {
-		panic(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		panic("login status " + string(resp.StatusCode))
-	}
-}
-
-func doDelete(track Track, username string) {
-	deleteURL := fmt.Sprintf("https://www.last.fm/user/%s/library/delete", username)
-
-	csrfCookie := findCookieForDomain(client.Jar, "https://www.last.fm", "csrftoken")
-
-	form := url.Values{}
-	form.Add("artist_name", track.Artist.Name)
-	form.Add("track_name", track.Name)
-	form.Add("timestamp", track.Date.Timestamp)
-	form.Add("csrfmiddlewaretoken", csrfCookie.Value)
-	form.Add("ajax", "1")
-
-	req, err := http.NewRequest(http.MethodPost, deleteURL, strings.NewReader(form.Encode()))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Referer", "https://www.last.fm")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Accept", "*/*")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	b, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(b))
-}
-
-func findCookieForDomain(cookieJar http.CookieJar, domain, name string) *http.Cookie {
-	domURL, err := url.Parse(domain)
-	if err != nil {
-		panic("Domain wouldn't parse")
-	}
-	for _, c := range cookieJar.Cookies(domURL) {
-		if c.Name == name {
-			return c
-		}
-	}
-	panic("Couldn't find cookie " + name)
 }
---
+++ web.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+)
+
+type LastFMWebClient struct {
+	client   http.Client
+	username string
+	password string
+	loginURL string
+}
+
+func NewLastFMWebClient(username, password string) (*LastFMWebClient, error) {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating cookiejar for web client HTTP client: %w", err)
+	}
+	webClient := LastFMWebClient{
+		client:   http.Client{Jar: jar},
+		username: username,
+		password: password,
+		loginURL: "https://secure.last.fm/login",
+	}
+	err = webClient.startSession()
+	if err != nil {
+		return nil, fmt.Errorf("Error starting LastFM web session: %w", err)
+	}
+	return &webClient, nil
+}
+
+func (c LastFMWebClient) startSession() error {
+	_, err := c.client.Get(c.loginURL)
+	if err != nil {
+		return fmt.Errorf("Failed to begin a LastFM web session: %w", err)
+	}
+
+	loginReq, err := c.buildLoginRequest()
+	if err != nil {
+		return fmt.Errorf("Failed to build login request: %w", err)
+	}
+	resp, err := c.client.Do(loginReq)
+	if err != nil {
+		return fmt.Errorf("Error performing LastFM web login: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Got unsuccessful status code (%d) on LastFM login", resp.StatusCode)
+	}
+	return nil
+}
+
+func (c LastFMWebClient) buildLoginRequest() (*http.Request, error) {
+	loginForm, err := c.buildLoginForm()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create the form required to do login: %w", err)
+	}
+	formContent := strings.NewReader(loginForm.Encode())
+	req, err := http.NewRequest(http.MethodPost, c.loginURL, formContent)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating a login request: %w", err)
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Referer", c.loginURL)
+	return req, nil
+}
+
+func (c LastFMWebClient) buildLoginForm() (url.Values, error) {
+	form := url.Values{}
+
+	csrfCookie := findCookieForDomain(c.client.Jar, "https://secure.last.fm", "csrftoken")
+	if csrfCookie == nil {
+		return form, errors.New("Failed to find csrf cookie to continue login")
+	}
+
+	form.Add("username_or_email", c.username)
+	form.Add("password", c.password)
+	form.Add("csrfmiddlewaretoken", csrfCookie.Value)
+	return form, nil
+}
+
+func findCookieForDomain(cookieJar http.CookieJar, domain, name string) *http.Cookie {
+	domURL, err := url.Parse(domain)
+	if err != nil {
+		panic("Domain wouldn't parse")
+	}
+	for _, c := range cookieJar.Cookies(domURL) {
+		if c.Name == name {
+			return c
+		}
+	}
+	panic("Couldn't find cookie " + name)
+}
+
+func (c LastFMWebClient) DeleteTrack(track Track) error {
+	deleteURL := fmt.Sprintf("https://www.last.fm/user/%s/library/delete", c.username)
+
+	csrfCookie := findCookieForDomain(c.client.Jar, "https://www.last.fm", "csrftoken")
+	if csrfCookie == nil {
+		return errors.New("Unable to find CSRF token for delete from established session")
+	}
+
+	form := url.Values{}
+	form.Add("artist_name", track.Artist.Name)
+	form.Add("track_name", track.Name)
+	form.Add("timestamp", track.Date.Timestamp)
+	form.Add("csrfmiddlewaretoken", csrfCookie.Value)
+	form.Add("ajax", "1")
+
+	req, err := http.NewRequest(http.MethodPost, deleteURL, strings.NewReader(form.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Referer", "https://www.last.fm")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept", "*/*")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Error reading delete response body: %w", err)
+	}
+	ajaxResp := struct {
+		Result bool `json:"result"`
+	}{}
+	err = json.Unmarshal(b, &ajaxResp)
+	if err != nil {
+		return fmt.Errorf("Failed to ready delete response JSON to check result: %w", err)
+	}
+	if !ajaxResp.Result {
+		return errors.New("Delete response indicate failure")
+	}
+	return nil
+}
